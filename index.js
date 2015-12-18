fb00fa97028cd09de58838525ac733dda17ede31
Eric Heikes <eric.heikes@banno.com>
2015-12-18 13:34:43 -0600

    Build out the plugin.

---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var gutil    = require('gulp-util');
+var linter   = require('ux-lint');
+var reporter = require('ux-lint/reporters/stylish');
+var through  = require('through2');
+
+var PLUGIN_NAME = 'gulp-ux-lint';
+
+module.exports = function(opts) {
+	var allLintErrors = [];
+	opts = opts || {};
+
+	return through.obj(function(file, enc, cb) {
+			if (file.isNull()) {
+				cb(null, file);
+				return;
+			}
+
+			if (file.isStream()) {
+				cb(new gutil.PluginError(PLUGIN_NAME, 'Streaming not supported'));
+				return;
+			}
+
+			var func = opts.fix ? 'fix' : 'check';
+			linter[func](file.path, opts, function(err, lintErrors) {
+				if (err) {
+					cb(new gutil.PluginError(PLUGIN_NAME, err));
+					return;
+				}
+				allLintErrors = allLintErrors.concat(lintErrors);
+				cb(null, file);
+			});
+		}, function(cb) {
+			reporter(allLintErrors);
+			cb();
+		});
+};
--- test/main.js
+++ test/main.js
@@ -1,13 +1,110 @@
 'use strict';
 
-var should = require('should');
+var assert     = require('stream-assert');
+var File       = require('gulp-util').File;
+var fs         = require('fs');
+var path       = require('path');
+var proxyquire = require('proxyquire');
+var should     = require('should');
+var sinon      = require('sinon');
+var Stream     = require('stream');
+var uxLint     = require('ux-lint');
 
 require('mocha');
 
+var testFile = 'fixtures/bad-javascript.js';
+
 describe('gulp-ux-lint', function() {
 
-	it('should stub', function() {
-		true.should.eql(true);
+	var stream, testBuffer;
+
+	var reporterSpy = sinon.spy();
+	sinon.spy(uxLint, 'check');
+	sinon.spy(uxLint, 'fix');
+
+	var linter = proxyquire('../', {
+		'ux-lint': uxLint,
+		'ux-lint/reporters/stylish': reporterSpy
+	});
+
+	beforeEach(function() {
+		reporterSpy.reset();
+		uxLint.check.reset();
+		uxLint.fix.reset();
+		stream = linter();
+		testBuffer = new File({
+			base: __dirname,
+			path: path.join(__dirname, testFile),
+			contents: new Buffer(fs.readFileSync(
+				path.join(__dirname, testFile),
+				{ encoding: 'utf8' }
+			).trim())
+		});
+	});
+
+	it('should call the reporter', function(done) {
+		stream
+			.pipe(assert.length(1))
+			.pipe(assert.first(function(file) {
+				file.should.eql(testBuffer);
+			}))
+			.pipe(assert.end(function() {
+				sinon.assert.calledOnce(reporterSpy);
+				(reporterSpy.firstCall.args[0].length).should.be.greaterThan(0);
+				done();
+			}));
+		stream.write(testBuffer);
+		stream.end();
+	});
+
+	it('should call the check() method', function(done) {
+		stream
+			.pipe(assert.length(1))
+			.pipe(assert.end(function() {
+				sinon.assert.calledOnce(uxLint.check);
+				done();
+			}));
+		stream.write(testBuffer);
+		stream.end();
+	});
+
+	it('should call the fix() method when { fix: true } is passed', function(done) {
+		stream = linter({ fix: true });
+		stream
+			.pipe(assert.length(1))
+			.pipe(assert.end(function() {
+				sinon.assert.calledOnce(uxLint.fix);
+				done();
+			}));
+		stream.write(testBuffer);
+		stream.end();
+	});
+
+	it('should work with no files', function(done) {
+		stream
+			.pipe(assert.length(1))
+			.pipe(assert.first(function(file) {
+				file.should.eql(new File());
+			}))
+			.pipe(assert.end(function() {
+				sinon.assert.calledOnce(reporterSpy);
+				done();
+			}));
+		stream.write(new File());
+		stream.end();
+	});
+
+	it('should emit an error with a streamed file', function(done) {
+		stream
+			.on('error', function(err) {
+				err.message.should.eql('Streaming not supported');
+				done();
+			});
+		stream.write(new File({
+			base: __dirname,
+			path: path.join(__dirname, testFile),
+			contents: new Stream()
+		}));
 	});
 
 });
